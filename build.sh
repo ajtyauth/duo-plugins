6a405b7b65ad5e8403e1e4f87bd6e5f71920d705
Jakub Fridrich <jakub.fridrich@klikpojisteni.cz>
2023-06-30 06:36:32 +0200

    Change certificate commit for v 1.1.3

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 VERSION=1.1.3
 
-git checkout -b b$VERSION
+git branch b$VERSION
+
+git checkout b$VERSION
 
 git clone -b $VERSION https://github.com/duosecurity/duo_universal_java.git
 
@@ -10,11 +12,15 @@ rm -rf ./duo_universal_java/.git
 
 cp -r ./duo_universal_java .
 
+git add .
+
 git commit -m "Change certificate commit for v $VERSION"
 
 git push
 
-git checkout -b master
+git branch master
+
+git checkout master
 
 git reset --hard 
 
