e909e1c6723f58225d5d39e43a0f5ce6cdd70792
Jakub Fridrich <jakub.fridrich@klikpojisteni.cz>
2023-06-30 06:25:19 +0200

    first commit

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+VERSION=1.1.3
+
+git branch b$VERSION
+
+git clone -b $VERSION https://github.com/duosecurity/duo_universal_java.git
+
+rm -rf ./duo_universal_java/.git
+
+cp -r ./duo_universal_java .
+
+
+git commit -m "Change certificate commit for v $VERSION"
+
+git push
+
+git branch master
+
+git reset --hard 
+
+git clean -f -d
